327600d6dde3e7dda857e779a7160eb290295293
Paolo Phan <phu.phan@zoi.de>
2021-06-07 23:29:23 +0700

    Update routes

--- src/screens/Root.jsx
+++ src/screens/Root.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { ScreensUser } from './User';
 
 const ScreensRoot = () => (
   <BrowserRouter>
     <Switch>
+      <Redirect exact from="/" to="/user" />
       <Route path="/user" component={ScreensUser} />
     </Switch>
   </BrowserRouter>
--- src/screens/User/index.jsx
+++ src/screens/User/index.jsx
@@ -2,7 +2,7 @@ export const ScreensUser = () => {
   return (
     <div>
       <h2>User List</h2>
-      <p>Try to not follow rules</p>
+      <p>Try to not follow rules 2</p>
     </div>
   );
 };
